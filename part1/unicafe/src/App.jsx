7ece8dbc062d20caed2b0eb5bd0fd3087d9ca5b7
KaptainCS3 <mbiapplegryn@gmail.com>
2024-09-19 00:28:54 +0100

    feat: complete and preview unicafe exercise

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import Header from "./component/Header";
+import Button from "./component/Button";
+import Statistics from "./component/Statistics";
+
+const Buttons = (props) => {
+  return (
+    <>
+      <Button text="good" handleAction={props.updateGoodState} />
+      <Button text="neutral" handleAction={props.updateNeutralState} />
+      <Button text="bad" handleAction={props.updateBadState} />
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const heading1 = "give feedback";
+  const heading2 = "statistics";
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const updateGoodState = () => {
+    setGood(good + 1);
+  };
+  const updateNeutralState = () => {
+    setNeutral(neutral + 1);
+  };
+  const updateBadState = () => {
+    setBad(bad + 1);
+  };
+
+  const all = good + neutral + bad;
+
+  const feedbackAverage = [1, 0, -1];
+
+  const goodAverage = feedbackAverage[0] * good;
+  const neutralAverage = feedbackAverage[1] * neutral;
+  const badAverage = feedbackAverage[2] * bad;
+
+  return (
+    <div>
+      <Header title={heading1} />
+      <Buttons
+        updateGoodState={updateGoodState}
+        updateNeutralState={updateNeutralState}
+        updateBadState={updateBadState}
+      />
+      <Header title={heading2} />
+      {good !== 0 || neutral !== 0 || bad !== 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          all={all}
+          average={(goodAverage + neutralAverage + badAverage) / all || 0}
+          positive={(good / all) * 100 || 0}
+        />
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
